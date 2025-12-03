0a5fecb82e9fb482cd6768907916a5e1b1b2fcd7
Abhinay <abhinaylakidi@gmail.com>
2025-12-03 21:40:56 +0530

    types of data types

---
+++ Data types/ex.py
@@ -0,0 +1,50 @@
+# What is a Variable?
+# A variable in Python is a reserved memory location to store values.
+# In other words, a variable in a Python program gives data to the computer for processing.
+# Variables are created when you assign a value to them.
+# For example:  
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+
+
+
+
+# Data type in python
+# data types are the classification or categorization of data items.
+# In Python, data types are used to define the type of a variable.  
+# Python has various built-in data types, including:
+# 1. Numeric Types: int, float, complex
+# 2. Sequence Types: list, tuple, range
+# 3. Text Type: str
+# 4. Set Types: set, frozenset
+# 5. Mapping Type: dict
+
+# primitive data types
+# Primitive data types are the most basic data types available within a programming language.
+# In Python, the primitive data types include:
+# 1. Integer (int): Represents whole numbers, e.g., 1, -5, 100
+a = 10
+print(type(a))
+
+# 2. Floating Point (float): Represents real numbers with decimal points, e.g., 3.14, -0.001, 2.0
+b = 3.14
+print(type(b))
+
+# 3. String (str): Represents sequences of characters, e.g., "Hello", 'Python123'
+c = "Hello, Python!"
+print(type(c))
+
+# 4. Boolean (bool): Represents truth values, either True or False
+d = True
+print(type(d))
+
+# 5. Complex (complex): Represents complex numbers, e.g., 2 + 3j
+e = 2 + 3j  
+print(type(e))
+
+# Non-primitive data types
+# Non-primitive data types are more complex data types that are built using primitive data types.
+# In Python, the non-primitive data types include:
