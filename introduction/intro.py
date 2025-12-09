7f37601f2b3edc414187517e118ee5cc2cc48821
Abhinay <abhinaylakidi@gmail.com>
2025-12-09 22:07:48 +0530

    loops

---
+++ introduction/intro.py
@@ -0,0 +1,30 @@
+# what is a language?
+# A language is a structured system of communication used by humans, consisting of spoken, written, or signed words and symbols. 
+# It allows individuals to express thoughts, emotions, and ideas, facilitating interaction and understanding within a community. 
+# Languages have their own rules of grammar, syntax, and vocabulary that govern how words are formed and combined to convey meaning.
+
+# what is a programme ?
+# A programme (or program) is a set of instructions written in a specific programming 
+# language that a computer can execute to perform a particular task or solve a problem.
+
+# what is a programming language?
+# A programming language is a formal language comprising a set of instructions that can be used to produce various kinds of output.
+# Programming languages are used in computer programming to implement algorithms and manipulate data structures. 
+
+# Examples of popular programming languages include Python, Java, C++, JavaScript, and Ruby.
+
+# what is python?
+# Python is a high-level, interpreted programming language known for its simplicity and readability.    
+# It was created by Guido van Rossum and first released in 1991. Python supports multiple programming paradigms,
+# including procedural, object-oriented, and functional programming. It has a large standard library and a vibrant ecosystem of third-party packages,
+# making it suitable for a wide range of applications, from web development and data analysis to artificial intelligence and scientific computing.  
+
+print("Welcome to the world of Python programming!")
+
+# why python?
+# 1. Easy to Learn and Use: Python has a simple syntax that is easy to read and write, making it an excellent choice for beginners.
+# 2. Versatile: Python can be used for web development, data analysis, artificial intelligence, scientific computing, automation, and more.
+# 3. Large Community: Python has a vast and active community that contributes to a rich ecosystem of libraries and frameworks.
+# 4. Cross-Platform: Python is available on various operating systems, including Windows, macOS, and Linux.
+# 5. Strong Support for Integration: Python can easily integrate with other languages and technologies, making it a flexible choice for diverse projects.
+
