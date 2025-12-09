56d6630767b608911cfd5880b4cccf02da3d4fb9
Abhinay <abhinaylakidi@gmail.com>
2025-12-09 22:05:45 +0530

    Conditional statements

---
+++ Conditional-statements/intro.py
@@ -0,0 +1,21 @@
+# Conditional statements in Python
+# Conditional statements are used to perform different actions based on different conditions.
+# The most common conditional statements in Python are if, elif, and else.
+
+# Syntax of conditional statements:
+# if condition1:
+#     # block of code to be executed if condition1 is true
+
+i=5
+if i==10:
+    print("i is 10")
+
+# elif condition2:
+#     # block of code to be executed if condition2 is true
+elif i>10:
+    print("i is greater than 10")
+
+# else:
+#     # block of code to be executed if both condition1 and condition2 are false
+else:
+    print("i is less than 10")
