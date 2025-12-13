8a7ce15ade1f0ee5461806119f36bab331d17691
Abhinay <abhinaylakidi@gmail.com>
2025-12-13 16:01:37 +0530

    loops

---
+++ loops/forloop.py
@@ -0,0 +1,25 @@
+# For Loop
+for i in range(5):
+    print(i)
+
+# Print numbers from 1 to 10
+
+for i in range(1,11):
+    print(i)
+
+# print even numbers from 1 to 20
+
+for i in range(2,21,2):
+    print(i)
+
+# print odd numbers from 1 to 20
+
+for i in range(1,20,2):
+    print(i)
+
+# print any table
+
+value=int(input("enter the number"))
+for i in range(1,11,1):
+    print(value,'*',i,'=',value*i)
+
