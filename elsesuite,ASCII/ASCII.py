3223b0e1743a16aef280e12afc0d0e3924269727
Abhinay <abhinaylakidi@gmail.com>
2025-12-18 18:33:30 +0530

    ASCII

---
+++ elsesuite,ASCII/ASCII.py
@@ -0,0 +1,6 @@
+# ASCII stands for American Standard Code for Information Interchange.
+# It is a character encoding standard for electronic communication. 
+# ASCII codes represent text in computers, telecommunications equipment, and other devices that use text.
+
+
+print(chr(56))
