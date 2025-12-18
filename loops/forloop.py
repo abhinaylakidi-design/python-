9fe009c07e985971dceb3f46e3f5e73a8ada7bf9
Abhinay <abhinaylakidi@gmail.com>
2025-12-18 17:37:22 +0530

    for loop

--- loops/forloop.py
+++ loops/forloop.py
@@ -23,3 +23,25 @@ value=int(input("enter the number"))
 for i in range(1,11,1):
     print(value,'*',i,'=',value*i)
 
+num=int(input("enter the number"))
+for i in range(1,11,1):
+    print(num,'*',i,'=',num*i)
+
+# print a plaendrome
+
+s=input("enter the string")
+rev=""
+for i in range(len(s)-1,-1,-1):
+    rev+=s[i]
+if s==rev:
+    print(s,"is a palendrome")
+else:
+    print(s,"is not a palendrome")
+
+
+# print the sum of first 10 natural numbers
+
+sum=0
+for i in range(1,11):
+    sum+=i
+print("the sum of first 10 natural numbers is",sum)
